06ff0cc1f710cab31ffdbfeb2371b475957f77a2
haram22 <kimhr9889@handong.ac.kr>
2023-12-27 20:53:34 +0900

    initial commit

---
+++ Sabotage/SabotageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SabotageApp.swift
+//  Sabotage
+//
+//  Created by 김하람 on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SabotageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
