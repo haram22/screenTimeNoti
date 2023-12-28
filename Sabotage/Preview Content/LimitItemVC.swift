167b971b218e88f09bd98b9e62fb974a950a16fd
haram22 <kimhr9889@handong.ac.kr>
2023-12-29 01:52:12 +0900

    [Feat] 제한서비스 지정하고, 지정한 서비스 보여주기

--- Sabotage/Preview Content/LimitItemVC.swift
+++ Sabotage/Preview Content/LimitItemVC.swift
@@ -1,4 +1,8 @@
 import UIKit
+import FamilyControls
+import SwiftUI
+import SnapKit
+import Then
 
 class LimitItemController: UIViewController {
     
@@ -13,7 +17,7 @@ class LimitItemController: UIViewController {
     let timeLabel2 = UILabel()
     
     let inputName = UITextField()
-    let appButton = UITextField()
+    let appButton = UIButton()
     
     // 유효성 검사 경고문
     let errorLabel: UILabel = {
@@ -27,7 +31,18 @@ class LimitItemController: UIViewController {
     
     override func viewDidLoad() {
         view.backgroundColor = .white
+        let detailView = DetailView()
+        let hostingController = UIHostingController(rootView: detailView)
         
+        // Add as a child of the current view controller
+        addChild(hostingController)
+        
+        // Add the SwiftUI view to the UIKit view hierarchy
+        view.addSubview(hostingController.view)
+        
+        
+        // Notify the hosting controller that it has been moved to the current view controller
+        hostingController.didMove(toParent: self)
         setupTapGesture()
         setupTextFields()
         
@@ -72,39 +87,36 @@ class LimitItemController: UIViewController {
             errorLabel.trailingAnchor.constraint(equalTo: inputName.trailingAnchor)
         ])
         
-        appButton.placeholder = "App"
-        appButton.textColor = .black
-        appButton.font = UIFont.systemFont(ofSize: 18)
-        appButton.layer.cornerRadius = 15
-        appButton.backgroundColor = .systemGray3
-        view.addSubview(appButton)
-        appButton.translatesAutoresizingMaskIntoConstraints = false
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.topAnchor.constraint(equalTo: view.topAnchor, constant: 250),
+            hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
         
-        NSLayoutConstraint.activate([
-            appButton.topAnchor.constraint(equalTo: inputName.bottomAnchor, constant: 50),
-            appButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            appButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            appButton.heightAnchor.constraint(equalToConstant: 60)
-        ])
-                
-        // 앱 하루 총 사용 시간 버튼
-        dailyBudgetButton.setTitle("하루 총 사용 시간", for: .normal)
-        dailyBudgetButton.setTitleColor(.systemGray, for: .normal)
-        dailyBudgetButton.titleLabel?.font = UIFont.systemFont(ofSize: 18)
-        dailyBudgetButton.contentHorizontalAlignment = .left
-        dailyBudgetButton.layer.cornerRadius = 15
-        dailyBudgetButton.backgroundColor = .systemGray3
-        dailyBudgetButton.titleEdgeInsets = UIEdgeInsets(top: -50, left: 5, bottom: 0, right: 50)
+        
         view.addSubview(dailyBudgetButton)
-        dailyBudgetButton.translatesAutoresizingMaskIntoConstraints = false
+        dailyBudgetButton.then {
+            $0.setTitle("하루 총 사용 시간", for: .normal)
+            $0.setTitleColor(.systemGray, for: .normal)
+            $0.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+            $0.contentHorizontalAlignment = .left
+            $0.layer.cornerRadius = 15
+            $0.backgroundColor = .systemGray3
+            $0.titleEdgeInsets = UIEdgeInsets(top: -50, left: 5, bottom: 0, right: 50)
+            $0.addTarget(self, action: #selector(dailyBudgetButtonTapped), for: .touchUpInside)
+        }
+        
+        // 앱 하루 총 사용 시간 버튼
+        dailyBudgetButton.snp.makeConstraints {
+            $0.top.equalTo(inputName.snp.bottom).offset(150)
+            $0.leading.equalToSuperview().offset(30)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.height.equalTo(100)
+        }
 
-        NSLayoutConstraint.activate([
-            dailyBudgetButton.topAnchor.constraint(equalTo: appButton.bottomAnchor, constant: 50),
-            dailyBudgetButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
-            dailyBudgetButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
-            dailyBudgetButton.heightAnchor.constraint(equalToConstant: 100)
-        ])
-        dailyBudgetButton.addTarget(self, action: #selector(dailyBudgetButtonTapped), for: .touchUpInside)
+        
         
         // 앱 하루 총 사용 시간 설정
         countDownDatePicker.datePickerMode = .countDownTimer
@@ -229,7 +241,7 @@ class LimitItemController: UIViewController {
         NSLayoutConstraint.activate([
             completeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             completeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            completeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -70),
+            completeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -30),
             completeButton.heightAnchor.constraint(equalToConstant: 70)
         ])
         completeButton.addTarget(self, action: #selector(completeButtonTapped), for: .touchUpInside)
@@ -403,6 +415,10 @@ class LimitItemController: UIViewController {
             errorLabel.isHidden = false
         }
     }
+    // MARK: - 제한할 앱 추가 버튼 클릭 시 실행되는 함수
+    @objc func appCardTapped() {
+        // 다른 페이지로 이동하는 로직을 여기에 구현
+    }
 }
 
 extension LimitItemController: UITextFieldDelegate {
@@ -425,6 +441,112 @@ extension LimitItemController {
     
     func setupTextFields() {
         inputName.delegate = self
-        appButton.delegate = self
+    }
+}
+
+
+struct DetailView: View {
+    @Environment(\.dismiss) private var dismiss
+    
+    @State private var settingIndex = 0
+    @State private var selection = FamilyActivitySelection()
+    @State private var isPresented = false
+    
+    private let columns = [
+        GridItem(.fixed(56)),
+        GridItem(.fixed(56)),
+        GridItem(.fixed(56)),
+        GridItem(.fixed(56)),
+        GridItem(.fixed(56))
+    ]
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            SelectAppContainerView()
+            Spacer()
+        }
+        .background(Color.white, ignoresSafeAreaEdges: .all)
+        .onAppear() {
+            selection = ScreenTimeVM.shared.selectionToDiscourage
+        }
+    }
+}
+
+extension DetailView {
+    private func SelectAppContainerView() -> some View {
+        // TODO::Pick interface
+        // VERSION 1
+        VStack(spacing: 0) {
+            HStack(alignment: .center, spacing: 0) {
+                Text("제한 중인 앱 목록")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Spacer()
+                Button("편집") { isPresented = true }
+                    .familyActivityPicker(
+                        isPresented: $isPresented,
+                        selection: $selection)
+                    .font(.subheadline)
+                    .padding(.horizontal, 10.0)
+                    .padding(.vertical, 4.0)
+                    .background(.white)
+                    .border(.white, width: 0)
+                    .cornerRadius(16)
+            }
+            .padding(.horizontal, 0.40)
+            // 앱 아이콘 나오는 부분
+            SelectedAppListView()
+        }
+    }
+    private func SelectedAppListView() -> some View {
+        VStack {
+            if (selection.applicationTokens.count > 0 || selection.categoryTokens.count > 0) {
+                LazyVGrid(columns: columns, alignment: .leading){
+                    if selection.applicationTokens.count > 0 {
+                        ForEach(Array(selection.applicationTokens), id: \.self) {
+                            token in
+                            HStack {
+                                Label(token)
+                                    .labelStyle(.iconOnly)
+                                    .scaleEffect(2.5)
+                            }
+                            .frame(width: 56, height: 56)
+                        }
+                    }
+                    if selection.categoryTokens.count > 0 {
+                        ForEach(Array(selection.categoryTokens), id: \.self) {
+                            token in
+                            HStack {
+                                Label(token)
+                                    .labelStyle(.iconOnly)
+                                    .scaleEffect(1.8)
+                            }
+                            .frame(width: 56, height: 56)
+                        }
+                    }
+                }
+                .padding(0.6)
+                .frame(maxWidth: .infinity, minHeight: 80)
+                .background(Color.blue)
+                .cornerRadius(16)
+            } else {
+                Text("선택된 앱이 없습니다.")
+                    .foregroundColor(.clear)
+                    .padding(0.6)
+                    .frame(maxWidth: .infinity, minHeight: 80)
+                    .background(Color.indigo)
+                    .cornerRadius(16)
+            }
+        }
+        .padding(.top, 0.50)
+        .padding(.horizontal, 0.50)
+    }
+}
+
+extension DetailView {
+    private func handleSavePlan() {
+        ScreenTimeVM.shared.selectionToDiscourage = selection
+        
+        dismiss()
     }
 }
