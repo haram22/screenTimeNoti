c1b2ea2598f76bee1353bb93988c620bdc526b5b
haram22 <kimhr9889@handong.ac.kr>
2023-12-30 17:45:13 +0900

    [feat] git error 해결

--- Sabotage/Main/ActionView/ActionTable/ActionTableView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ActionTableView.swift
-//  Sabotage
-//
-//  Created by 김하람 on 12/30/23.
-//
-
-import Foundation
---
+++ Sabotage/Main/LimitView/LimitModel.swift
@@ -0,0 +1,13 @@
+//
+//  LimitModel.swift
+//  Sabotage
+//
+//  Created by 김하람 on 12/30/23.
+//
+
+import Foundation
+
+struct LimitDummyDataType {
+    var title: String
+    var description: String
+}
