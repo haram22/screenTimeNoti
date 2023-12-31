5b1b0545fbf4da993d35263100da907d0ce6b280
haram22 <kimhr9889@handong.ac.kr>
2023-12-31 21:31:28 +0900

    banner successed

--- ScreenTimeReport/TotalActivityReport.swift
+++ ScreenTimeReport/TotalActivityReport.swift
@@ -8,44 +8,25 @@ import DeviceActivity
 import SwiftUI
 import UserNotifications
 
-
-// MARK: - 각각의 Device Activity Report들에 대응하는 컨텍스트 정의
 extension DeviceActivityReport.Context {
-    // If your app initializes a DeviceActivityReport with this context, then the system will use
-    // your extension's corresponding DeviceActivityReportScene to render the contents of the
-    // report.
-    /// 해당 리포트의 내용 렌더링에 사용할 DeviceActivityReportScene에 대응하는 익스텐션이 필요합니다.  ex) TotalActivityReport
     static let totalActivity = Self("Total Activity")
 }
 // MARK: - ram : dictionary for triger check
 var notificationSentForApps: [String: Bool] = [:]
 
-// MARK: - Device Activity Report의 내용을 어떻게 구성할 지 설정
 struct TotalActivityReport: DeviceActivityReportScene {
-    // Define which context your scene will represent.
-    /// 보여줄 리포트에 대한 컨텍스트를 정의해줍니다.
     let context: DeviceActivityReport.Context = .totalActivity
-    
-    // Define the custom configuration and the resulting view for this report.
-    /// 어떤 데이터를 사용해서 어떤 뷰를 보여줄 지 정의해줍니다. (SwiftUI View)
     let content: (ActivityReport) -> TotalActivityView
     var activityStartTime: Date?
-    /// DeviceActivityResults 데이터를 받아서 필터링
     func makeConfiguration(
         representing data: DeviceActivityResults<DeviceActivityData>) async -> ActivityReport {
-        // Reformat the data into a configuration that can be used to create
-        // the report's view.
         var totalActivityDuration: Double = 0 /// 총 스크린 타임 시간
         var list: [AppDeviceActivity] = [] /// 사용 앱 리스트
             let limitTime: Double = 2700
-            let specificLimitTime: Double = 1320
+            let specificLimitTime: Double = 2280 //38
             
-        /// DeviceActivityResults 데이터에서 화면에 보여주기 위해 필요한 내용을 추출해줍니다.
         for await eachData in data {
-            /// 특정 시간 간격 동안 사용자의 활동
             for await activitySegment in eachData.activitySegments {
-                
-                /// 활동 세그먼트 동안 사용자의 카테고리 별 Device Activity
                 for await categoryActivity in activitySegment.categories {
                     /// 이 카테고리의 totalActivityDuration에 기여한 사용자의 application Activity
                     for await applicationActivity in categoryActivity.applications {
@@ -57,8 +38,8 @@ struct TotalActivityReport: DeviceActivityReportScene {
                             scheduleNotification_each0(appName: applicationActivity.application.localizedDisplayName!)
                         }
                         if duration >= specificLimitTime && duration <= specificLimitTime + 60  { // 10 minutes
-//                            scheduleNotification_each1(appName: applicationActivity.application.localizedDisplayName!)
-                            setNotifications()
+                            scheduleNotification_each1(appName: applicationActivity.application.localizedDisplayName!)
+//                            setNotifications()
                             
                         }
                         if duration >= specificLimitTime + 60 && duration <= specificLimitTime + 120  { // 10 minutes
